1c778b9338e37217db9b8a62d071c48cd67ff0d1
allison-dahan <121320123+allison-dahan@users.noreply.github.com>
2023-01-08 13:23:25 -0800

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,73 @@
+// 1.
+
+// With the array below answer A-J
+
+/* 
+
+(hint for all of these questions: google "javascript array" + more context so if the question asks you to see if the array 
+includes something you would google "how to check if an awway includes an item in javascript") 
+
+*/
+
+// var fruits = ["Apple", "Banana", "Orange", "Strawberry", "Pineapple", "Pear", "Grapes", "Passionfruit", "Kiwi", "Mandarin"];
+
+// A: log out "Apple"
+
+
+// B: log out the length of fruits array
+
+
+// C: add "fruit cherry" to the beginning of the array
+
+
+
+// D: remove Mandarin from the end of the array
+
+
+// E: Log out the third element of the array (should output "Orange")
+
+
+
+// F: Add "Mango" to the end of the array
+
+
+// G: Remove "Pear" from the array
+
+
+// H: Check if "Kiwi" is included in the array (should output true)
+
+
+// I: Find the index of "Pineapple" in the array (should output 4)
+
+
+// J: Sort the array alphabetically and log out the sorted array (hint: google "javascript sort array")
+
+
+// K: to each item in that array add " is a fruit" and log out the new array (hint: ask for hint if you need")
+
+// 2.
+
+// Write A function that named isEven that takes a number as input (the paramater, ask me if you are confused) and returns true if the number is even and false if it is odd.
+
+
+
+// 3.
+// Write a function called noVowels that takes a string as an argument and 
+// returns a new string with all the vowels removed
+
+
+// reminder for how to write a function:
+
+// ... actually, try to look at ur notes and try to do it ur self, if you have trouble ask for help
+
+// 4
+
+// A function that takes a string and returns a new string with the first letter of each word capitalized.
+
+// We could do this one together
+
+
+
+
+
+
